5371ed4b4b6a3f550658450c02ed5aa5fb524921
Annasha Paul <annashapaul65855@gmail.com>
2025-07-01 18:16:24 +0530

    Create  Create server.js

---
+++ Create server.js
@@ -0,0 +1,21 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const taskRoutes = require('./routes/taskRoutes');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use('/api/tasks', taskRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch(err => console.error(err));
